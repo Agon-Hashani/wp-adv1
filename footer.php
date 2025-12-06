6b5c314adb6febc77e38ddcfee43430f164291ae
Agon-Hashani <agon.x.hashani@gmail.com>
2025-12-06 16:39:01 +0100

    full done

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 <footer class="footer text-center py-2 theme-bg-dark">
     <p class="copyright">Germanium Morina</p>
 </footer>
+<?php
+dynamic_sidebar("footer-1");
+?>
 </div>
 <?php wp_footer();?>
 
